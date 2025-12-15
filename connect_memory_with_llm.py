08b83c214bb0194fdc8da480de1ab946c701952e
karib shams <154616482+karibshams@users.noreply.github.com>
2025-12-16 02:46:47 +0600

    Update connect_memory_with_llm.py

--- connect_memory_with_llm.py
+++ connect_memory_with_llm.py
@@ -1,5 +1,5 @@
 from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader, JSONLoader, CSVLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 import os
